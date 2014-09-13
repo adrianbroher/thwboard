9b4f208fe35463979b523acfffee538c249c555e
Marcel Metz <mmetz@adrian-broher.net>
2014-09-13 22:58:45 +0200

    Unpack statistics extra feature

---
+++ inc/statscode/history.stats.php
@@ -0,0 +1,142 @@
+<?php
+/* $Id: history.stats.php,v 1.11 2003/10/12 01:08:25 dkreuer Exp $ */
+/*
+          ThWboard - PHP/MySQL Bulletin Board System
+        ==============================================
+            (c) 2000, 2001 by
+               Paul Baecher         <paul@thewall.de>
+               Felix Gonschorek   <funner@thewall.de>
+
+          download the latest version:
+            http://www.thwboard.de
+
+          This  program is  free  software;  you can
+          redistribute it and/or modify it under the
+          terms of the GNU General Public License as
+          published by the Free Software Foundation;
+          either  version 2 of  the License,  or (at
+          your option) any later version.
+
+        ==============================================
+
+*/
+
+if( !function_exists("isscached") )
+{
+function isscached($s, $e, $u)
+{
+	global $pref;
+	return mysql_num_rows(thwb_query("SELECT stat_id FROM " . $pref . "statcache WHERE stat_stime = $s AND stat_etime = $e AND stat_uid = $u LIMIT 1"));
+}
+}
+
+if( !function_exists("cachestats") )
+{
+function cachestats($s, $e, $u, $data)
+{
+	global $pref;
+	thwb_query("INSERT INTO " . $pref . "statcache (stat_stime, stat_etime, stat_uid, stat_month, stat_auser, stat_nuser, stat_nthread, stat_npost) VALUES (
+			" . $s . ",
+			" . $e . ",
+			" . $u . ",
+			'" . $data['month'] . "',
+			" . $data['auser'] . ",
+			" . $data['nuser'] . ",
+			" . $data['nthread'] . ",
+			" . $data['npost'] . "
+			)"); 
+}
+}
+
+if( !function_exists("getcachedstats") )
+{
+function getcachedstats($s, $e, $u)
+{
+	global $pref;
+	$r_query = thwb_query("SELECT stat_month as month, stat_auser as auser, stat_nuser as nuser, stat_nthread as nthread, stat_npost as npost FROM " .
+			$pref . "statcache WHERE stat_stime = $s AND stat_etime = $e AND stat_uid = $u DESC LIMIT 1");
+	
+	return mysql_fetch_array($r_query);
+}
+}
+
+
+if( $get_part == 'head' )
+{
+	$selectionpoints['history'] = array('base'=>'history', 'title'=>'History');
+}
+else
+{
+	$t_stats = new Template('./templates/' . $style['styletemplate'] . '/stats_history.html');
+	$monate = array("", "Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember");
+
+	$historyrow = '';
+	$data = array();
+	
+	$exit = false;
+	$negcount = 0;
+	$r_firstactive = thwb_query("SELECT userjoin FROM " . $pref . "user ORDER BY userjoin ASC LIMIT 1");
+	$maxtime = mysql_result($r_firstactive, 0);
+	mysql_free_result($r_firstactive);
+	$dc = 'CellA';
+	$t_row = new Template('./templates/' . $style['styletemplate'] . '/stats_historyrow.html');
+	$iscurmonth = true;
+	while( $exit != true )
+	{
+	  $endtime = mktime(0, 0, 0, date("m", time()) + 1 - $negcount, 0, date("Y", time()));
+	  $starttime = mktime(0, 0, 0, date("m", time()) - $negcount, 1, date("Y", time()));
+		
+		if( $iscurmonth || !isscached($starttime, $endtime, $g_user['userid']) )
+		{
+			$r_stats = thwb_query("SELECT count(u.userid) as nuser FROM " . $pref . "user as u WHERE u.userjoin > $starttime AND u.userjoin <= $endtime");
+			if( mysql_num_rows($r_stats) != 0 )
+			{
+				$data = mysql_fetch_array($r_stats);
+				$data['month'] = $monate[date("n", $starttime)] . " " . date("Y", $starttime);
+			}
+			$r_stats = thwb_query("SELECT count(p.userid) as auser FROM " . $pref . "post as p WHERE userid != 0 AND p.posttime > $starttime AND p.posttime <= $endtime GROUP BY p.userid");
+			$data['auser'] = mysql_num_rows($r_stats);
+
+			$r_stats = thwb_query("SELECT count(p.postid) as npost FROM " . $pref . "post as p WHERE p.posttime > $starttime AND p.posttime <= $endtime");
+			if( mysql_num_rows($r_stats) != 0 )
+			{
+				$data = array_merge($data, mysql_fetch_array($r_stats));
+			}
+			$r_stats = thwb_query("SELECT count(t.threadid) as nthread FROM " . $pref . "thread as t WHERE t.threadcreationtime > $starttime AND t.threadcreationtime <= $endtime");
+			if( mysql_num_rows($r_stats) != 0 )
+			{
+				$data = array_merge($data, mysql_fetch_array($r_stats));
+			}
+			if( !$iscurmonth )
+			{
+				cachestats($starttime, $endtime, $g_user['userid'], $data);
+			}
+		}
+		else
+		{
+			$data = getcachedstats($starttime, $endtime, $g_user['userid']);
+		}
+		eval($t_row->GetTemplate("historyrow"));
+		if( $dc == 'CellA' )
+		{
+			$dc = 'CellB';
+		}
+		else
+		{
+			$dc = 'CellA';
+		}
+		if( $starttime <= $maxtime )
+		{
+			$exit = true;
+		}
+		else
+		{
+			$negcount++;
+		}
+		$iscurmonth = false;
+	}
+
+	eval($t_stats->GetTemplate('stats'));
+}
+
+?>
---
+++ inc/statscode/top10.stats.php
@@ -0,0 +1,185 @@
+<?php
+/* $Id: top10.stats.php,v 1.5 2003/09/27 19:48:35 thed_o_n Exp $ */
+/*
+          ThWboard - PHP/MySQL Bulletin Board System
+        ==============================================
+            (c) 2000, 2001 by
+               Paul Baecher         <paul@thewall.de>
+               Felix Gonschorek   <funner@thewall.de>
+
+          download the latest version:
+            http://www.thwboard.de
+
+          This  program is  free  software;  you can
+          redistribute it and/or modify it under the
+          terms of the GNU General Public License as
+          published by the Free Software Foundation;
+          either  version 2 of  the License,  or (at
+          your option) any later version.
+
+        ==============================================
+
+*/
+
+if( $get_part == 'head' )
+{
+	$selectionpoints['top10']   = array('base'=>'top10',   'title'=>'Top 10 - Listen');
+}
+else
+{
+  $TStats = new Template('./templates/'.$style['styletemplate'].'/stats_top10.html');
+  $TRow = new Template('./templates/'.$style['styletemplate'].'/stats_top10row.html');
+
+  $stats_top10_row = '';
+  $data = array();
+
+  $P = new Permission($g_user['groupids']);
+
+  if($config['showpostslevel'] == 2 || $g_user['userisadmin'])
+    {
+
+  /** top 10 posters **/
+
+  $data['title'] = 'Top 10 Poster';
+  
+  $name = array();
+  $value = array();
+  $a_posters = array();
+
+  $i = 0;
+
+  $r_posters = thwb_query("SELECT userid, username, userposts FROM $pref"."user ORDER BY userposts DESC LIMIT 10");
+  while($a_posters = mysql_fetch_array($r_posters))
+    {
+      $name[$i] = '<a href="'.build_link('v_profile.php?userid='.$a_posters['userid']).'">'.$a_posters['username'].'</a>';
+      $value[$i] = $a_posters['userposts'];
+      $i++;
+    }
+
+  for(;$i <= 9; $i++)
+    {
+      $name[$i] = 'n/a';
+      $value[$i] = 'n/a';
+    }
+
+  mysql_free_result($r_posters);
+  
+  eval($TRow->GetTemplate("stats_top10_row"));
+ 
+  $stats_top10_row .= "<br>";
+    }
+
+  /** top 10 boards **/
+
+  $data['title'] = 'Top 10 Boards (nach Posts)';
+
+  $name = array();
+  $value = array();
+  $a_boards = array();
+
+  $i = 0;
+
+  $r_boards = thwb_query("SELECT b.boardid, b.boardname, b.boardposts, c.categoryname FROM $pref"."board AS b LEFT OUTER JOIN $pref"."category AS c ON c.categoryid = b.categoryid ORDER BY boardposts DESC LIMIT 10");
+  while($a_boards = mysql_fetch_array($r_boards))
+    {
+      $P->set_boardid($a_boards['boardid']);
+
+      if(!$P->has_permission(P_VIEW))
+	{
+	  continue;
+	}
+
+      $name[$i] = '<a href="'.build_link('board.php?boardid='.$a_boards['boardid']).'">'.$a_boards['boardname'].'</a>'.' (Kategorie: '.$a_boards['categoryname'].')';
+      $value[$i] = $a_boards['boardposts'];
+      $i++;
+    }
+
+  for(;$i <= 9; $i++)
+    {
+      $name[$i] = 'n/a';
+      $value[$i] = 'n/a';
+    }
+
+  mysql_free_result($r_boards);  
+
+  eval($TRow->GetTemplate("stats_top10_row"));
+
+  $stats_top10_row .= "<br>";
+
+
+  /** top 10 threads (by posts) **/
+
+  $data['title'] = 'Top 10 Threads (nach Posts)';
+
+  $name = array();
+  $value = array();
+  $a_postthreads = array();
+
+  $i = 0;
+
+  $r_postthreads = thwb_query("SELECT t.threadreplies, t.threadtopic, t.threadid, t.boardid, b.boardname FROM $pref"."thread AS t LEFT OUTER JOIN $pref"."board AS b ON t.boardid = b.boardid ORDER BY threadreplies DESC LIMIT 10");
+  while($a_postthreads = mysql_fetch_array($r_postthreads))
+    {
+      $P->set_boardid($a_postthreads['boardid']);
+
+      if(!$P->has_permission(P_VIEW))
+	{
+	  continue;
+	}
+
+      $name[$i] = '<a href="'.build_link('showtopic.php?threadid='.$a_postthreads['threadid']).'">'.parse_code($a_postthreads['threadtopic']).'</a> (Board: <a href="'.build_link('board.php?boardid='.$a_postthreads['boardid']).'">'.$a_postthreads['boardname'].'</a>)';
+      $value[$i] = $a_postthreads['threadreplies'] + 1;
+      $i++;
+    }
+
+  for(;$i <= 9; $i++)
+    {
+      $name[$i] = 'n/a';
+      $value[$i] = 'n/a';
+    }
+
+  mysql_free_result($r_postthreads);  
+
+  eval($TRow->GetTemplate("stats_top10_row"));
+
+  $stats_top10_row .= "<br>";
+
+  /** top 10 threads (by views) **/
+
+  $data['title'] = 'Top 10 Threads (nach Views)';
+
+  $name = array();
+  $value = array();
+  $a_viewthreads = array();
+
+  $i = 0;
+
+  $r_viewthreads = thwb_query("SELECT t.threadviews, t.threadtopic, t.threadid, t.boardid, b.boardname FROM $pref"."thread AS t LEFT OUTER JOIN $pref"."board AS b ON t.boardid = b.boardid ORDER BY threadviews DESC LIMIT 10");
+  while($a_viewthreads = mysql_fetch_array($r_viewthreads))
+    {
+      $P->set_boardid($a_viewthreads['boardid']);
+
+      if(!$P->has_permission(P_VIEW))
+	{
+	  continue;
+	}
+
+      $name[$i] = '<a href="'.build_link('showtopic.php?threadid='.$a_viewthreads['threadid']).'">'.parse_code($a_viewthreads['threadtopic']).'</a> (Board: <a href="'.build_link('board.php?boardid='.$a_viewthreads['boardid']).'">'.$a_viewthreads['boardname'].'</a>)';
+      $value[$i] = $a_viewthreads['threadviews'];
+      $i++;
+    }
+
+  for(;$i <= 9; $i++)
+    {
+      $name[$i] = 'n/a';
+      $value[$i] = 'n/a';
+    }
+
+  mysql_free_result($r_viewthreads);  
+
+  eval($TRow->GetTemplate("stats_top10_row"));
+
+  eval($TStats->GetTemplate("stats"));
+}
+
+?>
