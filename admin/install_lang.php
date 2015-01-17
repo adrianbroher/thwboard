efd96426e8c929941d8c412720c50e27b656c5da
Marcel Metz <mmetz@adrian-broher.net>
2015-01-17 17:50:15 +0100

    Update license banner in installer and updater scripts

--- admin/install_lang.php
+++ admin/install_lang.php
@@ -1,18 +1,25 @@
 <?php
-/*
-          phpInstaller - PHP Script Installer
-        ==============================================
-          (c) 2000-2004 by ThWboard Development Group
-
-
-          This  program is  free  software;  you can
-          redistribute it and/or modify it under the
-          terms of the GNU General Public License as
-          published by the Free Software Foundation;
-          either  version 2 of  the License,  or (at
-          your option) any later version.
-
-        ==============================================
+/**
+ * ThWboard - PHP/MySQL Bulletin Board System
+ * ==========================================
+ *
+ * Copyright (C) 2000-2006 by ThWboard Development Group
+ * Copyright (C) 2015 by Marcel Metz
+ *
+ * This file is part of ThWboard
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program;  If not, see <http://www.gnu.org/licenses/>.
  */
 
 $a_lang = [
