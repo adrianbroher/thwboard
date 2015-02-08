e9a90af348f9ab5270319dd6009e900a44518af3
Marcel Metz <mmetz@adrian-broher.net>
2015-02-08 22:53:07 +0100

    Add a ThWboard model manipulation context for behat
    This context should contain methods to set up the ThWboard model
    properly for functional tests.

---
+++ tests/behat-bootstrap/ThWboardModelFeatureContext.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * ThWboard - PHP/MySQL Bulletin Board System
+ * ==========================================
+ *
+ * Copyright (C) 2015 by Marcel Metz
+ *
+ * This file is part of ThWboard
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program;  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use Behat\Behat\Context\Context;
+
+/**
+ * The behat features context to prepare and manipulate the ThWboard model.
+ */
+class ThWboardModelFeatureContext implements Context
+{
+}
