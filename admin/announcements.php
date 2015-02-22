d1ff972f4dbd2152b28320dbecd6cce1a9d40336
Marcel Metz <mmetz@adrian-broher.net>
2015-02-22 19:56:23 +0100

    Replace concatenated HTML output with inline php code

--- admin/announcements.php
+++ admin/announcements.php
@@ -56,41 +56,37 @@ SQL
         $boards[] = $board;
     }
 
-    print '<form name="announcements" method="post" action="'.htmlspecialchars($handler).'">
-  <table border="0" cellspacing="1" cellpadding="2">
-    <tr>
-      <td><label for="announcement-title">Title</label></td>
-      <td>
-        <input class="tbinput" id="announcement-title" type="text" name="announcement-title" size="45" value="' . htmlspecialchars($announcement->title) . '">
-      </td>
-    </tr>
-    <tr>
-      <td valign="top"><label for="announcement-body">Body</label></td>
-      <td>
-        <textarea class="tbinput" id="announcement-body" name="announcement-body" cols="60" rows="8">' . htmlspecialchars($announcement->body) . '</textarea>
-        Note: You can use ThWboard Code in announcements.
-      </td>
-    </tr>
-    <tr>
-      <td valign="top"><label for="announcement-boardids">Boards</label></</td>
-      <td>
-        <SELECT class="tbinput" id="announcement-boardids" name="announcement-boardids[]" size="8" multiple>';
-
-    foreach ($boards as $board) {
-        print "<option value=\"".htmlspecialchars($board->ID)."\" " . (stristr($announcement->boardIDs, ";".$board->ID.";") != false ? 'selected="selected"' : '') . ">".htmlspecialchars($board->name)."</option>";
-    }
-
-    print '</select>
-      </td>
-    </tr>
-    <tr>
-      <td>&nbsp;</td>
-      <td>
-        <input type="submit" name="submit" value="Save">
-      </td>
-    </tr>
-  </table>
-</form>';
+?>
+<form name="announcements" method="post" action="<?= htmlspecialchars($handler) ?>">
+    <table border="0" cellspacing="1" cellpadding="2">
+        <tr>
+            <td><label for="announcement-title">Title</label></td>
+            <td><input class="tbinput" id="announcement-title" type="text" name="announcement-title" size="45" value="<?= htmlspecialchars($announcement->title) ?>"></td>
+        </tr>
+        <tr>
+            <td valign="top"><label for="announcement-body">Body</label></td>
+            <td>
+                <textarea class="tbinput" id="announcement-body" name="announcement-body" cols="60" rows="8"><?= htmlspecialchars($announcement->body) ?></textarea><br>
+                Note: You can use ThWboard Code in announcements.
+            </td>
+        </tr>
+        <tr>
+            <td valign="top"><label for="announcement-boardids">Boards</label></td>
+            <td>
+                <select class="tbinput" id="announcement-boardids" name="announcement-boardids[]" size="8" multiple>
+<?php foreach ($boards as $board): ?>
+                    <option value="<?= htmlspecialchars($board->ID) ?>"<?= (stristr($announcement->boardIDs, ";".$board->ID.";") != false ? ' selected="selected"' : '') ?>><?= htmlspecialchars($board->name) ?></option>
+<?php endforeach ?>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td><input type="submit" name="submit" value="Save"></td>
+        </tr>
+    </table>
+</form>
+<?php
 }
 
 
@@ -100,9 +96,6 @@ SQL
  * ########################################################################################
  */
 if ('ListNews' == $_GET['action']) {
-    print "<a href=\"announcements.php?action=new&session=".$session."\">Add announcement</a>";
-    print "<h3>Announcements</h3>";
-
     $r_announcement = query(
 <<<SQL
 SELECT
@@ -117,15 +110,26 @@ ORDER BY
 SQL
     );
 
-    print "<ul id=\"announcements\">";
-
+    $announcements = [];
     while ($announcement = mysql_fetch_object($r_announcement)) {
-        print "<li>";
-        print date("d.m.Y H:i: ", $announcement->ctime) . $announcement->title . " [ <a href=\"announcements.php?action=edit&session=".$session."&amp;id=".$announcement->ID."\" title=\"Edit announcement ".htmlspecialchars($announcement->title)."\">edit</a> ] [ <a href=\"announcements.php?action=delete&session=".$session."&amp;id=".$announcement->ID."\" title=\"Delete announcement ".htmlspecialchars($announcement->title)."\">delete</a> ]</a><br>";
-        print "</li>";
+        $announcements[] = $announcement;
     }
-
-    print "</ul>";
+?>
+<a href="announcements.php?action=new&amp;session=<?= htmlspecialchars($session) ?>">Add announcement</a>
+<h3>Announcements</h3>
+<ul id="announcements">
+<?php foreach ($announcements as $announcement): ?>
+    <li>
+        <?= date("d.m.Y H:i: ", $announcement->ctime) ?>
+        <?= htmlspecialchars($announcement->title) ?>
+        <ul class="actions">
+            <li><a href="announcements.php?action=edit&amp;id=<?= htmlspecialchars($announcement->ID) ?>&amp;session=<?= $session ?>" title="Edit announcement <?= htmlspecialchars($announcement->title) ?>">edit</a></li>
+            <li><a href="announcements.php?action=delete&amp;id=<?= htmlspecialchars($announcement->ID) ?>&amp;session=<?= $session ?>" title="Delete announcement <?= htmlspecialchars($announcement->title) ?>">delete</a></li>
+        </ul>
+    </li>
+<?php endforeach ?>
+</ul>
+<?php
 }
 
 
@@ -308,7 +312,7 @@ SQL
         print "Announcement has been deleted!";
     } else {
 ?>
-<form method="post" action="announcements.php?action=delete&amp;id=<?= $_GET['id'] ?>&amp;session=<?= $session ?>">
+<form method="post" action="announcements.php?action=delete&amp;id=<?= htmlspecialchars($_GET['id']) ?>&amp;session=<?= htmlspecialchars($session) ?>">
     Do you really want to delete the announcement?
     <input type="submit" name="submit" value="Delete">
 </form>
