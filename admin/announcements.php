12ab6da42c909ca218dbd25dcd9e747973f4c9eb
Marcel Metz <mmetz@adrian-broher.net>
2015-02-26 20:37:09 +0100

    Replace mysql_fetch_(assoc|array) with mysql_fetch_object
    Unify the mysql fetch method used in the announcement administrator
    interface.

--- admin/announcements.php
+++ admin/announcements.php
@@ -34,7 +34,7 @@ function EditboxDecode($string)
     return $string;
 }
 
-function NewsForm($handler, $news)
+function NewsForm($handler, $announcement)
 {
     global $pref;
 
@@ -58,13 +58,13 @@ SQL
     <tr>
       <td><label for="announcement-title">Title</label></td>
       <td>
-        <input class="tbinput" id="announcement-title" type="text" name="announcement-title" size="45" value="' . htmlspecialchars($news['newstopic']) . '">
+        <input class="tbinput" id="announcement-title" type="text" name="announcement-title" size="45" value="' . htmlspecialchars($announcement->title) . '">
       </td>
     </tr>
     <tr>
       <td valign="top"><label for="announcement-body">Body</label></td>
       <td>
-        <textarea class="tbinput" id="announcement-body" name="announcement-body" cols="60" rows="8">' . htmlspecialchars($news['newstext']) . '</textarea>
+        <textarea class="tbinput" id="announcement-body" name="announcement-body" cols="60" rows="8">' . htmlspecialchars($announcement->body) . '</textarea>
         Note: You can use ThWboard Code in announcements.
       </td>
     </tr>
@@ -74,7 +74,7 @@ SQL
         <SELECT class="tbinput" id="announcement-boardids" name="announcement-boardids[]" size="8" multiple>';
 
     foreach ($boards as $board) {
-        print "<option value=\"".htmlspecialchars($board->ID)."\" " . (stristr($news['boardid'], ";".$board->ID.";") != false ? 'selected="selected"' : '') . ">".htmlspecialchars($board->name)."</option>";
+        print "<option value=\"".htmlspecialchars($board->ID)."\" " . (stristr($announcement->boardIDs, ";".$board->ID.";") != false ? 'selected="selected"' : '') . ">".htmlspecialchars($board->name)."</option>";
     }
 
     print '</select>
@@ -100,13 +100,13 @@ if ('ListNews' == $_GET['action']) {
     print "<a href=\"announcements.php?action=new&session=".$session."\">Add announcement</a>";
     print "<h3>Announcements</h3>";
 
-    $r_news = query(
+    $r_announcement = query(
 <<<SQL
 SELECT
-    newsid,
-    newstopic,
-    newstext,
-    newstime
+    newsid AS ID,
+    newstopic AS title,
+    newstext AS body,
+    newstime AS ctime
 FROM
     {$pref}news
 ORDER BY
@@ -116,9 +116,9 @@ SQL
 
     print "<ul id=\"announcements\">";
 
-    while ($news = mysql_fetch_array($r_news)) {
+    while ($announcement = mysql_fetch_object($r_announcement)) {
         print "<li>";
-        print date("d.m.Y H:i: ", $news['newstime']) . $news['newstopic'] . " [ <a href=\"announcements.php?action=edit&session=".$session."&amp;id=".$news['newsid']."\" title=\"Edit announcement ".htmlspecialchars($news['newstopic'])."\">edit</a> ] [ <a href=\"announcements.php?action=delete&session=".$session."&amp;id=".$news['newsid']."\" title=\"Delete announcement ".htmlspecialchars($news['newstopic'])."\">delete</a> ]</a><br>";
+        print date("d.m.Y H:i: ", $announcement->ctime) . $announcement->title . " [ <a href=\"announcements.php?action=edit&session=".$session."&amp;id=".$announcement->ID."\" title=\"Edit announcement ".htmlspecialchars($announcement->title)."\">edit</a> ] [ <a href=\"announcements.php?action=delete&session=".$session."&amp;id=".$announcement->ID."\" title=\"Delete announcement ".htmlspecialchars($announcement->title)."\">delete</a> ]</a><br>";
         print "</li>";
     }
 
@@ -150,7 +150,7 @@ if ('edit' == $_GET['action']) {
             $r_boards = query(
 <<<SQL
 SELECT
-    boardid
+    boardid AS ID
 FROM
     {$pref}board
 WHERE
@@ -159,15 +159,15 @@ SQL
             );
 
             $boardIDs = [];
-            while ($board = mysql_fetch_assoc($r_boards)) {
-                $boardIDs[] = $board['boardid'];
+            while ($board = mysql_fetch_object($r_boards)) {
+                $boardIDs[] = $board->ID;
             }
 
             if (empty($boardIDs)) {
                 print "The announcement needs to visible in at least one board.";
             } else {
-                $newsTopic = addslashes(EditboxDecode($_POST['announcement-title']));
-                $newsBody = addslashes($_POST['announcement-body']);
+                $title = addslashes(EditboxDecode($_POST['announcement-title']));
+                $body  = addslashes($_POST['announcement-body']);
 
                 $boardIDs = ';' . implode(';', $boardIDs) . ';';
 
@@ -176,8 +176,8 @@ SQL
 UPDATE
     {$pref}news
 SET
-    newstext  = '{$newsBody}',
-    newstopic = '{$newsTopic}',
+    newstext  = '{$body}',
+    newstopic = '{$title}',
     boardid   = '{$boardIDs}'
 WHERE
     newsid = {$_GET['id']}
@@ -188,22 +188,21 @@ SQL
             }
         }
     } else {
-        $r_news = query(
+        $r_announcement = query(
 <<<SQL
 SELECT
-    newsid,
-    boardid,
-    newstopic,
-    newstext,
-    newstime
+    newsid AS ID,
+    newstopic AS title,
+    newstext AS body,
+    boardid AS boardIDs
 FROM
     {$pref}news
 WHERE
     newsid = {$_GET['id']}
 SQL
         );
-        $news = mysql_fetch_array($r_news);
-        NewsForm('announcements.php?action=edit&id='.$news['newsid'].'&session='.$session, $news);
+        $announcement = mysql_fetch_object($r_announcement);
+        NewsForm('announcements.php?action=edit&id='.$announcement->ID.'&session='.$session, $announcement);
     }
 }
 
@@ -230,7 +229,7 @@ if ('new' == $_GET['action']) {
             $r_boards = query(
 <<<SQL
 SELECT
-    boardid
+    boardid AS ID
 FROM
     {$pref}board
 WHERE
@@ -239,15 +238,15 @@ SQL
             );
 
             $boardIDs = [];
-            while ($board = mysql_fetch_assoc($r_boards)) {
-                $boardIDs[] = $board['boardid'];
+            while ($board = mysql_fetch_object($r_boards)) {
+                $boardIDs[] = $board->ID;
             }
 
             if (empty($boardIDs)) {
                 print "The announcement needs to visible in at least one board.";
             } else {
-                $newsTopic = addslashes($_POST['announcement-title']);
-                $newsBody = addslashes($_POST['announcement-body']);
+                $title = addslashes($_POST['announcement-title']);
+                $body  = addslashes($_POST['announcement-body']);
 
                 $boardIDs = ';' . implode(';', $boardIDs) . ';';
 
@@ -257,13 +256,13 @@ INSERT INTO
     {$pref}news
 (
     newstopic,
-    boardid,
     newstext,
+    boardid,
     newstime
 ) VALUES (
-    '{$newsTopic}',
+    '{$title}',
+    '{$body}',
     '{$boardIDs}',
-    '{$newsBody}',
     UNIX_TIMESTAMP()
 )
 SQL
@@ -273,7 +272,13 @@ SQL
             }
         }
     } else {
-        NewsForm('announcements.php?action=new&session='.$session, []);
+        $announcement = (object)[
+            'ID' => 0,
+            'title' => '',
+            'body' => '',
+            'boardIDs' => ';'
+        ];
+        NewsForm('announcements.php?action=new&session='.$session, $announcement);
     }
 }
 
